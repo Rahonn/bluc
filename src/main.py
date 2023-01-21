e6c56868f8c0b2a35f8e4985dc307167c65333cd
aixoio <aixoio@yahoo.com>
2023-01-22 10:31:27 +1100

    Bug fix

--- src/main.py
+++ src/main.py
@@ -70,7 +70,7 @@ def modeH():
     print(
         "Loops\t-\tUse LOOPSTART [index varname] [times] replace [times] with @ for a infinite number of times. Use BREAKLOOP to break out of a loop. Use ENDLOOP to end a loop\t-\t\n\tLOOP index 5\n\tPRINTVAR index\n\tRUNIF $index == 3 |BREAKLOOP|PASS|\n\tENDLOOP")
     print("Read file\t-\tUse to read files\t-\tREADFILE [outputvar] [filepath]")
-    print("Write file\t-\tUse to read files\t-\tWRITEFILE [inputvar] [filepath]")
+    print("Write file\t-\tUse to write files\t-\tWRITEFILE [inputvar] [filepath]")
     print("String Concat\t-\tUse to add 2 vars to together as text. If you add 2 and 3 you will get 23. If your add Hello and World you will get Hello World. Replace [arg1] or [arg2] with plain text or for a var put a $ at the start pf the name\t-\tCONCAT [outputvarname] |[arg1]|[arg2]|")
     print()
     print(f"\t{Fore.RED}Comands Options{Fore.RESET}")
